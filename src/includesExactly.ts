9d7e7c4fbc5b7345a1a0d65d6c5f07425d425abf
Vincent Pretre <vincent.pretre@gmail.com>
2023-01-17 14:21:00 +0100

    Add simple function to ease comparing arrays

---
+++ src/includesExactly.ts
@@ -0,0 +1,16 @@
+export default function includesExactly<T>(actual: ReadonlyArray<T>, expected: ReadonlyArray<T>): {
+  missingElements: ReadonlyArray<T>,
+  extraElements: ReadonlyArray<T>
+} {
+  const jsonActual = actual.map(item => JSON.stringify(item))
+  const jsonExpected = expected.map(item => JSON.stringify(item))
+
+  return {
+    missingElements: findMissing(jsonActual, jsonExpected).map(item => JSON.parse(item) as T),
+    extraElements: findMissing(jsonExpected, jsonActual).map(item => JSON.parse(item) as T)
+  }
+}
+
+function findMissing(actual: ReadonlyArray<string>, expected: ReadonlyArray<string>): ReadonlyArray<string> {
+  return expected.filter(item => !actual.includes(item))
+}
